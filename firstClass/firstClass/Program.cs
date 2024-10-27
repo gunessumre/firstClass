c8cdc5b8c34c2367c2f6fb51ce947f7c76992e7e
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-10-27 08:56:44 +0300

    Add files via upload

---
+++ firstClass/firstClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Person
+{
+    
+    public string name { get; set; }
+    public string surName { get; set; }
+    public DateTime bornDate { get; set; }
+    
+    public void printInfo()
+    {
+        Console.WriteLine($"Ad: {name}, Soyad: {surName}, Doğum Tarihi: {bornDate.ToShortDateString()}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Person student1 = new Person { name = "Martin", surName = "Eden", bornDate = new DateTime(1889, 6, 12) };
+        Person student2 = new Person { name = "Jude", surName = "Fawley", bornDate = new DateTime(1875, 9, 18) };
+        Person teacher1 = new Person { name = "Rodion Romanovic", surName = "Raskolnikov", bornDate = new DateTime(1846, 2, 22) };
+        Person teacher2 = new Person { name = "Zeynep", surName = "Çelik", bornDate = new DateTime(1900, 11, 5) };
+
+        
+        student1.printInfo();
+        student2.printInfo();
+        teacher1.printInfo();
+        teacher2.printInfo();
+    }
+}
